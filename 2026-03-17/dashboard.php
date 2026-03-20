e0e7de6aee84af85c37f4191134348a8fe8e004c
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-03-19 23:36:30 -0300

    feat: lista de carrinhos

--- 2026-03-17/dashboard.php
+++ 2026-03-17/dashboard.php
@@ -1,8 +1,9 @@
 <?php
-session_start();
+
 require 'middleware/auth.php';
 
-$dados = json_decode(file_get_contents("data/products.json"));
+$dados = json_decode(file_get_contents("data/products.json"), true);
+$produtos = $dados['products'];
 
 ?>
 <!DOCTYPE html>
@@ -11,10 +12,28 @@ $dados = json_decode(file_get_contents("data/products.json"));
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Dashboard</title>
     <style>
         .min-height-screen {
             min-height: 100vh;
+        }
+
+        .products-container {
+            display: grid;
+            gap: 4px;
+            grid-template-columns: repeat(4, minmax(13rem, 1fr));
+        }
+
+        .produto-image {
+            aspect-ratio: 16 / 9;
+        }
+
+        @media only screen and (width < 900px) {
+            .products-container {
+                display: grid;
+                gap: 4px;
+                grid-template-columns: repeat(1, minmax(13rem, 1fr));
+            }
         }
     </style>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
@@ -33,17 +52,28 @@ $dados = json_decode(file_get_contents("data/products.json"));
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" href="carrinho.php">
-                            <i class="bi bi-cart text-white me-2"></i>Carrinho
+                            <i class="bi bi-cart me-2"></i>Carrinho
                         </a>
                     </li>
                 </ul>
-                <span class="me-2"><?= $_SESSION['user_email'] ?></span>
-                <a href="logout.php" class="link-danger" role="button">Sair</a>
+                <span class="me-4 text-white"><?= $_SESSION['user_email'] ?></span>
+                <div class="nav-item"><a href="logout.php" class="link-danger text-decoration-none fw-bold" role="button">Sair</a></div>
             </div>
         </div>
     </nav>
-    <div class="container">
-        <span>Test</span>
+    <div class="container mt-4 products-container">
+        <?php foreach ($produtos as $produto): ?>
+            <div class="card">
+                <img src="<?= $produto['image'] ?>" class="card-img-top produto-image" alt="exemplo">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $produto['name'] ?></h5>
+                    <p class="card-text">
+                        R$<?php echo $produto['price'] ?>
+                    </p>
+                    <a href="#" class="btn btn-primary">Adicionar ao carrinho</a>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
--- 2026-03-17/login.php
+++ 2026-03-17/login.php
@@ -1,23 +1,36 @@
 <?php
 session_start();
 
-define('TRINTA_DIAS', 3600 * 24 * 30);
+if (isset($_SESSION['user_email'])) {
+    header("Location: dashboard.php");
+    die();
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    define('TRINTA_DIAS', 3600 * 24 * 30);
+
     $email = $_POST['email'] ?? null;
-    // $password = $_POST['senha'] ?? null;
     $remember_me = ($_POST['lembreme'] ?? null) == 'on';
-
     // Sem banco de dados, a senha não é utilizada.
-    if (isset($email)) {
-        $_SESSION['user_email'] = $email;
+    $password = $_POST['senha'] ?? null;
 
-        if ($remember_me) {
-            setcookie('user_email', $email, time() + TRINTA_DIAS);
-        }
-
-        header("Location: dashboard.php");
+    if (strlen($password) < 8) {
+        header('Location: login.php?err=A senha deve ter 8 caracteres ou mais.');
+        die();
     }
+
+    if (empty($email)) {
+        header('Location: login.php');
+        die();
+    }
+
+    $_SESSION['user_email'] = $email;
+
+    if ($remember_me) {
+        setcookie('user_email', $email, time() + TRINTA_DIAS);
+    }
+
+    header("Location: dashboard.php");
 }
 
 ?>
@@ -32,6 +45,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         .min-height-screen {
             min-height: 100vh;
         }
+
+        .w-login-card {
+            width: clamp(170px, 100%, 24rem);
+        }
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
@@ -39,18 +56,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body style="background-color: #f6f6f6;">
     <div class="container d-flex flex-column justify-content-center align-items-center min-height-screen">
-        <form action="#" method="post" class="card p-4" style="width: 24rem;">
+        <form action="#" method="post" class="card p-4 w-login-card">
             <div class="mb-3">
                 <label for="email" class="form-label">E-mail</label>
                 <input type="email" class="form-control" id="email" name="email" placeholder="nome@exemplo.com" value="<?= $_COOKIE['user_email'] ?? '' ?>">
             </div>
             <div class="mb-3">
                 <label for="senha" class="form-label">Senha</label>
-                <input type="password" class="form-control" id="senha">
+                <input type="password" class="form-control" id="senha" name="senha" required>
+                <div class="text-danger" id="errMessage">
+                    <?= $_GET['err'] ?>
+                </div>
             </div>
             <div class="form-check mb-3">
                 <input class="form-check-input" type="checkbox" value="on" id="lembreme" name="lembreme">
-                <label class="form-check-label" for="lembrebe">
+                <label class="form-check-label" for="lembreme">
                     Lembre-me
                 </label>
             </div>
